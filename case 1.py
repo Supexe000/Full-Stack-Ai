9cc0a0dbdc110c0ca2c42b56aded19fcc4dffd44
Supexe000 <supexegamer@gmail.com>
2025-06-23 18:20:58 +0500

    week 1

---
+++ case 1.py
@@ -0,0 +1,36 @@
+#first program
+print("start here")
+
+name = input("Enter your name: ")   
+age= input("Enter your age: ") 
+class1 = input("Enter your class: ")
+height = input("Enter your height: ")   
+print("You are " + age + " years old, in class " + class1 + ", and your height is " + height + ".")     
+
+ #addition
+print('sum:',int(age) + float(height) +int(class1))
+# subtraction
+print ('subtraction:', int(age) - float(height) - int(class1))           
+# multiplication
+print('multiplication:', int(age) * float(height) * int(class1))
+# division
+print('division:', int(age) / float(height) / int(class1))
+# modulus
+print('modulus:', int(age) % float(height) % int(class1))       
+# exponentiation
+print('exponentiation:', int(age) ** float(height) ** int(class1))
+# floor division
+print('floor division:', int(age) // float(height) // int(class1))
+ 
+# equal to operator
+print('equal to:', int(age) == float(height) == int(class1))            
+# not equal to operator 
+print('not equal to:', int(age) != float(height) != int(class1))
+# greater than operator
+print('greater than:', int(age) > float(height) > int(class1))
+# less than operator
+print('less than:', int(age) < float(height) < int(class1))
+# greater than or equal to operator
+print('greater than or equal to:', int(age) >= float(height) >= int(class1))
+# less than or equal to operator
+print('less than or equal to:', int(age) <= float(height) <= int(class1))
---
+++ slicing.py
@@ -0,0 +1,30 @@
+name = "Zain Zahid"
+
+print("name:", name)
+
+#sting for upper case
+print("upper case:", name.upper())
+# string for lower case
+print("lower case:", name.lower())  
+# string for title case
+print("title case:", name.title())  
+# string for capitalize
+print("capitalize:", name.capitalize()) 
+# string for swapcase
+print("swapcase:", name.swapcase())
+# string for count
+print("count of 'a':", name.count('a'))
+
+name = "Zain Zahid"
+print(name[0:4:])
+print(name[0:4:2])
+
+print(name[::-1]) 
+
+n=len(name)
+print("length of name:", n)
+
+
+for i in name:
+    print(i)
+
