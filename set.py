dc3b4d33cbabe3b8e0d4239e810a211b46ec3261
Supexe000 <supexegamer@gmail.com>
2025-06-25 11:01:24 +0500

    week 1

---
+++ set.py
@@ -0,0 +1,16 @@
+set = {1, 2, 3, 4, 5,"lahore","pakistan"}
+print(set)
+
+print(type(set)) 
+
+set.add(6)
+print(set)
+
+set.remove("lahore")  
+print(set)
+
+set.discard("pakistan")
+print(set)    
+
+
+
---
+++ tuples.py
@@ -0,0 +1,14 @@
+my_data = ("zain", 20, "Pakistan", "lahore")
+
+print(my_data)
+print(type(my_data))
+
+print(my_data[3])
+
+print(my_data[2])
+
+
+print(my_data[-2])
+
+
+
